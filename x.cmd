bf225897bfbf5822d547fef6798ec246e01c9b82
drhoangzp <114561932+drhoangzp@users.noreply.github.com>
2025-09-24 16:17:58 +0700

    Update x.cmd

--- x.cmd
+++ x.cmd
@@ -8,14 +8,16 @@ echo 1. Kich hoat Windows - Office
 echo 2. Kiem tra Windows 10/11
 echo 3. Kiem tra Windows 7
 echo 4. Tuy chinh Windows
+echo 5. Tat Windows Update
 echo 0. Thoat
 echo ===============================
-set /p choice=Nhap lua chon (1-4, 0 de thoat): 
+set /p choice=Nhap lua chon (1-5, 0 de thoat): 
 
 if "%choice%"=="1" goto get
 if "%choice%"=="2" goto checkinfo
 if "%choice%"=="3" goto checkwin7
 if "%choice%"=="4" goto winutil
+if "%choice%"=="5" goto update
 if "%choice%"=="0" goto exit
 
 echo Lua chon khong hop le. Vui long nhap lai.
@@ -70,5 +72,29 @@ set /p back=Nhap 0 de quay lai:
 if "%back%"=="0" goto menu
 goto wait0a
 
+:update
+cls
+echo ==== Tat Windows Update====
+echo Dang chay script...
+@echo off
+:: Kiểm tra quyền Admin bằng cách ghi vào thư mục system
+>nul 2>&1 net session
+if %errorLevel% NEQ 0 (
+    echo Dang yeu cau quyen Administrator...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+net stop wuauserv
+net stop bits
+sc config wuauserv start= disabled
+sc config bits start= disabled
+echo ---------------------------------------
+echo Nhan 0 de quay lai menu chinh
+:wait0a
+set /p back=Nhap 0 de quay lai: 
+if "%back%"=="0" goto menu
+goto wait0a
+
 :exit
 exit
+
